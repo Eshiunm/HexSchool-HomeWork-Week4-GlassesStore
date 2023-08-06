20ab1704e6c7f2efc297aaad233728545bd4cbd2
Eshiunm <eshiunm@gmail.com>
2023-08-06 15:41:47 +0800

    更改分店圖片

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/HexSchool-HomeWork-Week4-GlassesStore/',
+  base: '/HexSchool_GlassesStore/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
